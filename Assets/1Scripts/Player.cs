59d8c3e3398574d5c9f7d6e2a5df95e3fe6caa15
hooony23 <42611599+hooony23@users.noreply.github.com>
2020-11-09 15:15:05 +0900

    초기 프로젝트 파일 업로드
    간단한 환경 임시 구성후 업로드하였습니다

---
+++ Assets/1Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // 기본 움직임
+    float hAxis;
+    float vAxis;
+    Vector3 moveVec;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+}
