5ac498378a5e226b80c337a1cee9ef9698414e01
HwangSunGyu <71918623+HwangSunGyu@users.noreply.github.com>
2020-11-14 16:26:39 +0900

    맵 클리어 아이템 구현
    1.아이템(박스) 습득시 다음 맵(Scene)으로 전환
    * 다음맵이 미구현이라 습득시 오류발생

---
+++ Assets/1Scripts/MapClearItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapClearItem : MonoBehaviour
+{
+    // enum 선언으로 열거형 타입으로 아이템 분류
+    public enum MapClear { ClearItem };
+    public MapClear ItemType;
+
+}
+
--- Assets/1Scripts/Player.cs
+++ Assets/1Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,6 +12,10 @@ public class Player : MonoBehaviour
     float vAxis;
     Vector3 moveVec;
     public float speed;
+
+    bool eDown; // E키를 누르는 것을 표시
+    bool getItem = false; // 클리어 아이템 획득 표시
+    GameObject nearObject; // 아이템 습득로직을 위한 오브젝트 정의
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +29,14 @@ public class Player : MonoBehaviour
         GetInput();
         Move();
         Turn();
+        GetItem();
     }
 
     void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        eDown = Input.GetKeyDown(KeyCode.E); //E키를 통한 아이템 습득
     }
 
     void Move()
@@ -41,5 +48,35 @@ public class Player : MonoBehaviour
     void Turn()
     {
         transform.LookAt(transform.position + moveVec);
+    }
+    void GetItem() // 아이템 획득을 위한 로직
+    {
+        if (eDown && nearObject != null)
+        { //아이템을 먹었을 때 실행하는 문장
+            if (nearObject.tag == ("Item"))
+            {
+                getItem = true;
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other) //플레이어 범위에 아이템이 인식할 수 있는지 확인
+    {
+        if (other.CompareTag("Item"))
+        {
+            nearObject = other.gameObject;
+            if (getItem == true)
+            {
+                SceneManager.LoadScene("First Map"); //getItem이 true일경우 다음 맵으로 이동
+                Destroy(nearObject);// 이동과 동시에 아이템 오브젝트가 사라짐
+            }
+        }
+
+    }
+    private void OnTriggerExit(Collider other) //플레이어 범위에 아이템이 벗어났는지 확인
+    {
+        if (other.CompareTag("Item"))
+        {
+            nearObject = null;
+        }
     }
 }
