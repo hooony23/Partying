31f8537fb590f484f0c05cec8ebafd5b5a7ac0e4
hooony23 <42611599+hooony23@users.noreply.github.com>
2021-03-21 22:48:55 +0900

    (성훈) 레이저 발사 파티클 제작
    Partycle시스템 이용, 레이저 발사 구현

---
+++ Assets/Scripts/Boss/BossDronePattern.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDronePattern : MonoBehaviour
+{
+
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
