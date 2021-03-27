fc5832510c74532dd0f643079392fe3a4084464b
hooony23 <42611599+hooony23@users.noreply.github.com>
2021-03-27 20:52:17 +0900

    레이저 패턴 제작
    1. 옥타 레이저 애니메이션
    2. 차지 레이저 부분, 효과 추가

--- Assets/Scripts/Boss/BossDrone.cs
+++ Assets/Scripts/Boss/BossDrone.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class BossDrone : MonoBehaviour
 {
-    ParticleSystem chargingL, octaL;
-
     void Start()
     {
-        GameObject chargingLaser = GameObject.Find("Charging Laser");
-        chargingL = chargingLaser.GetComponent<ParticleSystem>();
-        chargingL.Stop();
-
-        GameObject octaLaser = GameObject.Find("Octa Laser");
-        octaL = octaLaser.GetComponent<ParticleSystem>();
-        octaL.Stop();
+        
     }
     
     
@@ -22,12 +14,13 @@ public class BossDrone : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Keypad0))
         {
-            chargingL.Play();
+            BossPattern.Instance.ChargingLaser();
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
-            octaL.Play();
+            BossPattern.Instance.OctaLaser();
+
         }
 
     }
---
+++ Assets/Scripts/Boss/BossPattern.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPattern : MonoBehaviour
+{
+    ParticleSystem chargingL, octaL;
+    Animator anim;
+    private static BossPattern instance = null;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public static BossPattern Instance
+    {
+        get
+        {   if (null == instance) return null;
+            return instance;
+        }
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        GameObject chargingLaser = GameObject.Find("Charging Laser");
+        chargingL = chargingLaser.GetComponent<ParticleSystem>();
+        chargingL.Stop();
+
+        GameObject octaLaser = GameObject.Find("Octa Laser");
+        octaL = octaLaser.GetComponent<ParticleSystem>();
+        octaL.Stop();
+    }
+
+    public void ChargingLaser()
+    {
+        chargingL.Play();                   // 파티클 시스템 플레이
+    }
+
+    public void OctaLaser()
+    {
+        octaL.Play();                       // 파티클 시스템
+        anim.SetTrigger("OctaLaser1");      // 레이저 총구 각도 변환 애니메이션
+    }
+}
