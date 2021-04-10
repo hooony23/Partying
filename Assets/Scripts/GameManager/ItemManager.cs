e43938e0cb367f0f39f83c5869fb7f649f7ee415
HwangSunGyu <71918623+HwangSunGyu@users.noreply.github.com>
2021-04-10 17:34:25 +0900

    (순규) 플레이어 Item충돌 처리
    1. 플레이어와 Item 충돌 구현
    2. 플레이어 버프, 아이템 획득 처리(이동속도, 체력회복 2개 구현)
    * 아이템효과 적용은 RaidPlayerUtil에서 정보를 가져옴

--- Assets/Scripts/GameManager/ItemManager.cs
+++ Assets/Scripts/GameManager/ItemManager.cs
@@ -4,44 +4,94 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    enum Item { WindItem, AttckItem , HeartItem ,ReloadSpeedItem, HealthmaxItem, ResurrectionItem }
+    enum Item { WindItem, AttckItem, HeartItem, ReloadSpeedItem, HealthmaxItem, ResurrectionItem }
     Item Wind = Item.WindItem;
     Item Attck = Item.AttckItem;
     Item Heart = Item.HeartItem;
     Item ReloadSpeed = Item.ReloadSpeedItem;
     Item Healthmax = Item.HealthmaxItem;
     Item Resurrection = Item.ResurrectionItem;
-    public void ItemActive(string Itemname, GameObject ItemObject) {
+    public void ItemActive(string Itemname, GameObject ItemObject, GameObject PlayUser)
+    {
 
-       // Debug.Log(Itemname);
-       // Destroy(ItemObject);
-        if (Itemname.Equals(Wind.ToString())) {
+        RaidPlayerUtil User = PlayUser.gameObject.GetComponent<RaidPlayerUtil>();
+        GameObject Itemactivetime = ItemObject.transform.Find("Itemmodle").gameObject;
+        float UserHealth = User.PlayerHealth;
+
+        if (Itemname.Equals(Wind.ToString()))
+        {
             Debug.Log("바람 아이템");
-            Destroy(ItemObject);
+            StartCoroutine(Itembuff(Itemname, User, ItemObject, 30f));
+            Itemactivetime.SetActive(false);
         }
         if (Itemname.Equals(Attck.ToString()))
         {
             Debug.Log("공격 아이템");
-            Destroy(ItemObject);
-        }
-        if (Itemname.Equals(Heart.ToString()))
-        {
-            Debug.Log("체력회복 아이템");
-            Destroy(ItemObject);
+            StartCoroutine(Itembuff(Itemname, User, ItemObject, 10f));
+            Itemactivetime.SetActive(false);
         }
         if (Itemname.Equals(ReloadSpeed.ToString()))
         {
             Debug.Log("장전속도 아이템");
-            Destroy(ItemObject);
+            StartCoroutine(Itembuff(Itemname, User, ItemObject, 10f));
+            Itemactivetime.SetActive(false);
+        }
+        if (Itemname.Equals(Heart.ToString()))
+        {
+            Debug.Log("체력회복 아이템");
+            StartCoroutine(Itemapply(Itemname, User, ItemObject));
+            Itemactivetime.SetActive(false);
         }
         if (Itemname.Equals(Healthmax.ToString()))
         {
+            StartCoroutine(Itemapply(Itemname, User, ItemObject));
             Debug.Log("피 영구증가 아이템");
-            Destroy(ItemObject);
+            Itemactivetime.SetActive(false);
         }
         if (Itemname.Equals(Resurrection.ToString()))
         {
+            StartCoroutine(Itemapply(Itemname, User, ItemObject));
             Debug.Log("부활 아이템");
+            Itemactivetime.SetActive(false);
+        }
+    }
+    public IEnumerator Itembuff(string Itemname2, RaidPlayerUtil User, GameObject ItemObject, float time)
+    {
+        if (Itemname2.Equals(Wind.ToString())) {//이동속도 2배, 30초
+            User.PlayerSpeed = 42f;
+            yield return new WaitForSeconds(time);
+            User.PlayerSpeed = 14f;
+            Destroy(ItemObject);
+        }
+        if (Itemname2.Equals(Attck.ToString())) {// 공격력 2배,10초
+           //플레이어 데미지 증가 [스크립트 끌어오기(미구현)] 
+            yield return new WaitForSeconds(time);
+            //플레이어 데미지 감소 [스크립트 끌어오기(미구현)] 
+            Destroy(ItemObject);
+        }
+        if (Itemname2.Equals(ReloadSpeed.ToString())) { //공격속도 3배, 10초
+            //플레이어 공격속도 증가 [스크립트 끌어오기(미구현)] 
+            yield return new WaitForSeconds(time);
+            //플레이어 공격속도 감소 [스크립트 끌어오기(미구현)] 
+            Destroy(ItemObject);
+        }
+    }
+    public IEnumerator Itemapply(string Itemname2, RaidPlayerUtil User, GameObject ItemObject)
+    {
+        if (Itemname2.Equals(Heart.ToString())) {// 플레이어 체력 회복
+            User.RaidplayerHealth += 100f;
+            Debug.Log(User.RaidplayerHealth);
+            yield return new WaitForSeconds(0.1f);
+            Destroy(ItemObject);
+        }
+        if (Itemname2.Equals(Attck.ToString())) {// 플레이어 체력 영구증가
+            //아이템 오브젝트와 충돌한 플레이어 최대체력 증가 [스크립트 끌어오기(미구현)] 
+            yield return new WaitForSeconds(0.1f);
+            Destroy(ItemObject);
+        }
+        if (Itemname2.Equals(ReloadSpeed.ToString())) { //플레이어 랜덤 부활
+            //아이템 오브젝트와 충돌한 플레이어외 랜덤플레이어 부활 [스크립트 끌어오기(미구현)] 
+            yield return new WaitForSeconds(0.1f);
             Destroy(ItemObject);
         }
     }
--- Assets/Scripts/Map/Item/ItemRotation.cs
+++ Assets/Scripts/Map/Item/ItemRotation.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class ItemRotation : ItemManager
 {
-
+    bool buff = false;
     void Update()
     {
         transform.Rotate(new Vector3(0, 20f, 0) * Time.deltaTime, Space.World);
     }
 
     private void OnTriggerStay(Collider other) //아이템과 플레이어가 충돌시 발생하는 이벤트
-    { 
+    {
         if (other.CompareTag("Player"))
         {
             GameObject BuffItem = this.gameObject;
             string BuffItemname = this.gameObject.name;
-            ItemActive(BuffItemname, BuffItem);
+            GameObject PlayUser = other.gameObject;
+            if (!buff)
+            {
+                buff = true;
+                StartCoroutine(Asdf(BuffItemname, PlayUser, BuffItem));
+            }
 
         }
+
+    }
+    public IEnumerator Asdf(string BuffItemname, GameObject PlayUser, GameObject BuffItem)
+    {
+        ItemActive(BuffItemname, BuffItem, PlayUser);
+        yield return new WaitForSeconds(5f);
+        buff = false;
 
     }
 }
