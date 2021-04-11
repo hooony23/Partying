a45b68e2f9092ca0c664ed8e4c09813e482d553f
sehyanOh <skine134@gmail.com>
2021-04-11 18:14:45 +0900

    test용 코드 추가.

--- Assets/Scripts/GameManager/GameManagerUtil.cs
+++ Assets/Scripts/GameManager/GameManagerUtil.cs
@@ -29,19 +29,23 @@ namespace GameManager
         }
         protected void InitializeRaid()
         {
-            InitUserList();
-
             GameObject playerCamera = Instantiate(Resources.Load("Player/CameraArm"), Vector3.zero, Quaternion.identity) as GameObject;
             playerCamera.name = Resources.Load("Player/CameraArm").name;
 
-            GameObject Boss = Instantiate(Resources.Load("Raid/Boss/BossPrefab/Boss"), new Vector3(150,0,150), Quaternion.identity) as GameObject;
-            Boss.name = Resources.Load("Raid/Boss/BossPrefab/Boss").name;
+
+            //TODO: Test용 소환 삭제해야함
+            GameObject player = Instantiate(Resources.Load("Player/Player"), new Vector3(150,0,150), Quaternion.identity) as GameObject;
+            player.name = Resources.Load("Player/Player").name;
             
             GameObject Map = Instantiate(Resources.Load("Raid/Map/RaidTerrain"), Vector3.zero, Quaternion.identity) as GameObject;
             Map.name = Resources.Load("Raid/Map/RaidTerrain").name;
 
             GameObject ItemManager = Instantiate(Resources.Load("Raid/Item/ItemManager"), Vector3.zero, Quaternion.identity) as GameObject;
             ItemManager.name = Resources.Load("Raid/Item/ItemManager").name;
+            
+            InitUserList();
+            GameObject Boss = Instantiate(Resources.Load("Raid/Boss/BossPrefab/Boss"), new Vector3(150,30,150), Quaternion.identity) as GameObject;
+            Boss.name = Resources.Load("Raid/Boss/BossPrefab/Boss").name;
         }
         protected void DelUser()
         {
@@ -76,10 +80,11 @@ namespace GameManager
         }
         protected void InitUserList()
         {
-            if (NetworkInfo.playersInfo.Count == 0)
+            if (NetworkInfo.playersInfo.Count > 0)
             {
                 foreach (var playerUuid in NetworkInfo.playersInfo.Keys)
                 {
+                    Debug.Log(playerUuid);
                     PlayerList.Add(GameObject.Find(playerUuid));
                 }
             }
--- Assets/Scripts/ItemManager/ItemManager.cs
+++ Assets/Scripts/ItemManager/ItemManager.cs
@@ -9,8 +9,8 @@ namespace ItemManager
         {
             var num = -1;
             //if(!NewtworkInfo.ItemRespawn.Equal(""))
-            foreach (EItem item in Enum.GetValues(typeof(EItem))) { if (item.ToString().Equals("itemName")) { num = (int)item; break; } }
-            Instantiate(Resources.Load(ItemLocation[num]) as GameObject);
+            // foreach (EItem item in Enum.GetValues(typeof(EItem))) { if (item.ToString().Equals("itemName")) { num = (int)item; break; } }
+            // Instantiate(Resources.Load(ItemLocation[num]) as GameObject);
         }
     }
 }
